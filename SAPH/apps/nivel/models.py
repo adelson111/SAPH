9fc5df0d2ea8eb875cff3a9a01496f19ce34d706
Matheus Kaio <matheuskaio2000@gmail.com>
2019-05-09 21:17:10 -0300

    Adicionando funcionario a setor. Adicionando nivel inferior e nivel superior. Adicionando gerente a um setor

--- SAPH/apps/nivel/models.py
+++ SAPH/apps/nivel/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
 # Create your models here.
+from apps.funcionario.models import Funcionario
+
 
 
 class Nivel(models.Model):
-    # nivelSuperior = models.ForeignKey(Nivel, on_delete=models.PROTECT)
-    # nivelInferior = models.ForeignKey(Nivel, on_delete=models.PROTECT)
-    # funcionario = models.ForeignKey()
-    pass
+    nivelSuperior = models.ForeignKey('self', null=True, blank=True, on_delete=models.PROTECT, related_name='nivelsuperior')
+    nivelInferior = models.ForeignKey('self', null=True, blank=True, on_delete=models.PROTECT, related_name='nivelinferior')
+    funcionario = models.ForeignKey(Funcionario, on_delete=models.PROTECT)
--- SAPH/apps/setor/models.py
+++ SAPH/apps/setor/models.py
@@ -2,15 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+from django.urls import reverse
+
 from apps.funcionario.models import Funcionario
+from apps.nivel.models import Nivel
 
 
 class Setor(models.Model):
     nome = models.CharField(max_length=20)
-    funcionario = models.ManyToManyField(Funcionario, blank=True)
+    funcionario = models.ManyToManyField(Funcionario, blank=True, null=True)
+    nivel = models.ForeignKey(Nivel, blank=True, null=True, on_delete=models.PROTECT)
+    gerente = models.ForeignKey(Funcionario, blank=False, null=True, on_delete=models.SET_NULL, related_name='gerentesetor')
 
     def __str__(self):
         return self.nome
 
     class Meta:
         verbose_name_plural = 'Setores'
+

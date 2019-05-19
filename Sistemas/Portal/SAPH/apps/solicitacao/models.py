9b47dd394cb6e4ec786f0218c746a0eb037a946e
Márcio Douglas <marciodouglasxavier@gmail.com>
2019-05-18 21:36:16 -0300

    Cadastro de Item com ajax

--- Sistemas/Portal/SAPH/apps/solicitacao/models.py
+++ Sistemas/Portal/SAPH/apps/solicitacao/models.py
@@ -12,6 +12,7 @@ class Solicitacao(models.Model):
     tipo = models.CharField(max_length=25)
     descricao = models.CharField(max_length=300, null = False)
     itens = models.ManyToManyField(Item,null=True, related_name='itens_solicitacao')
+    status = models.CharField(max_length = 25, null = False)
 
     def get_absolute_url(self):
         return reverse('cadasrtrar_solicitacao')
--- Sistemas/Portal/SAPH/apps/solicitacao/views.py
+++ Sistemas/Portal/SAPH/apps/solicitacao/views.py
@@ -11,43 +11,8 @@ from apps.solicitacao.models import Item, Solicitacao
 
 
 class CadastrarSolicitacao(CreateView):
-    form1 = ItemForm
-    form2 = SolicitacaoForm
-    template_name = 'solicitacao/solicitacao_form.html'
-
-    def get(self, request, *args, **kwargs):
-        formOne = self.form1(prefix='form1')
-        formTwo = self.form2(prefix='form2')
-        return render(request, self.template_name, {
-            'form1': formOne,
-            'form2': formTwo
-        })
-
-    def post(self, request, *args, **kwargs):
-        nome_itens = request.POST.getlist('form1-nome')
-        data_dicts = [{'nome':nome}for nome in nome_itens]
-        itens = []
-        for data in data_dicts:
-            formOne = self.form1(data)
-            if formOne.is_valid():
-                pk = formOne.save()
-                itens.append(get_object_or_404(Item, pk=pk.pk))
-
-
-        formTwo = self.form2(request.POST,prefix="form2")
-
-        if formTwo.is_valid():
-            solicitacao = formTwo.save()
-            for item in itens:
-                solicitacao.itens.add(item)
-
-            return redirect('cadasrtrar_solicitacao')
-
-        return render(request, self.template_name, {
-            'form1': self.form1(prefix='form1'),
-            'form2': self.form2(prefix='form2')
-        })
-
+    model = Solicitacao
+    fields = ['tipo', 'descricao', 'itens']
 
 class ListarSolicitacao(ListView):
     model = Solicitacao
@@ -75,11 +40,4 @@ class  CadastrarItem(CreateView):
             item = form.save()
             data['nome'] = item.nome
             data['id'] = item.id
-        return JsonResponse(data)
-
-class  ListarItem(ListView):
-    def  get(self, request):
-        itens =  list(Item.objects.all().values())
-        data =  dict()
-        data['nome'] = itens
         return JsonResponse(data)

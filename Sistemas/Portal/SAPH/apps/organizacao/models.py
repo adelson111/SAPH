9d24c7261fa0b6d1ec94d9d1b788dbab4c700225
matheuskaio <matheuskaio@github.com>
2019-05-30 14:19:59 -0300

    Adicionando mascara em CPF e CNPJ

--- Sistemas/Portal/SAPH/apps/organizacao/models.py
+++ Sistemas/Portal/SAPH/apps/organizacao/models.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 
 class Organizacao(models.Model):
     nome = models.CharField(max_length=80)
-    cnpj = models.CharField(max_length=15)
+    cnpj = models.CharField(max_length=20)
     endereco = models.CharField(max_length=50)
     telefone = models.CharField(max_length=15)
     situacao = models.BooleanField(default=True)

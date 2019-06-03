e511abbac27b1d1f59cb2bf39b504ec7e2dcbc61
Moisés Costa Lins <costa.lins@academico.ifrn.edu.br>
2019-06-03 17:41:25 -0300

    tela de login

--- Sistemas/UI/SAPH/routes/solicitacoes.js
+++ Sistemas/UI/SAPH/routes/solicitacoes.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.get('/new',(req, res, next)=>{
   var Client = require('node-rest-client').Client;
   var client = new Client();
-  client.get("http://localhost:8084/SAPH/SAPH/solicitacao/listar", function (data, response) {
+  client.get("http://localhost:8080/SAPH/SAPH/solicitacao/listar", function (data, response) {
     res.render('new', {
       title: 'Solicitacoes - SAPH',
       tipos: data,

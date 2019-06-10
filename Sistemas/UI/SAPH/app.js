421e3d4ce9f29ad4ba8a6da2f6e0b7e7c56ec477
Moisés Costa Lins <costa.lins@academico.ifrn.edu.br>
2019-06-10 16:36:22 -0300

    finalizando interface de deçegações

--- Sistemas/UI/SAPH/app.js
+++ Sistemas/UI/SAPH/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var solicitacoesRouter = require('./routes/solicitacoes');
+var delegacoesRouter = require('./routes/delegacoes');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/solicitacoes', solicitacoesRouter);
+app.use('/delegacoes', delegacoesRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));

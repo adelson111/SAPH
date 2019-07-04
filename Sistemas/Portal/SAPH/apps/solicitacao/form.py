88d6afdd7a7d0414e96f4830f921b603efa7759e
matheuskaio <matheuskaio@github.com>
2019-07-03 21:32:43 -0300

    Resolvendo bug de campo nas solicitações e delegações.

--- Sistemas/Portal/SAPH/apps/solicitacao/form.py
+++ Sistemas/Portal/SAPH/apps/solicitacao/form.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import ModelForm
 
+from apps.item.models import Item
 from apps.nivel.models import Nivel
 from apps.solicitacao.models import Solicitacao
 
@@ -14,9 +15,13 @@ class CreateSolicitacao(ModelForm):
 
     def __init__(self, organizacao,*args, **kwargs):
         niveis = Nivel.objects.filter(organizacao=organizacao)
+        itens = Item.objects.filter(organizacao=organizacao)
+
         super(CreateSolicitacao, self).__init__(*args, **kwargs)
+
         self.fields['nivel'].widget.attrs = {'class': 'custom-select'}
         self.fields['itens'].widget.attrs = {'class': 'custom-select'}
+        self.fields['itens'].queryset = itens
         self.fields['nivel'].queryset = niveis
 
     class Meta:

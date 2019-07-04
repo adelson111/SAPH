79edfe7d8aed230c950d98d96167585b309a04c3
unknown <20171134040013@nc377317.ifrn.local>
2019-07-04 08:30:04 -0300

    Removendo bug no comentário

--- Sistemas/UI/SAPH/routes/solicitacoes.js
+++ Sistemas/UI/SAPH/routes/solicitacoes.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 var Client = require('node-rest-client').Client;
 var client = new Client();
-var server_solicitacao_delegacao = "http://localhost:8080/SAPH/saph/solicitacao-delegacao/";
-var server_tipo_solicitacao_delegacao = "http://localhost:8080/SAPH/saph/tipo-solicitacao-delegacao/";
+var server_solicitacao_delegacao = "http://10.193.1.48:8080/SAPH/saph/solicitacao-delegacao/";
+var server_tipo_solicitacao_delegacao = "http://10.193.1.48:8080/SAPH/saph/tipo-solicitacao-delegacao/";
 
 module.exports = (io)=>{
   router.get('/nova',(req, res, next)=>{

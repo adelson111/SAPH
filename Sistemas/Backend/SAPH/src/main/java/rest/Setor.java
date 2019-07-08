1c8e60e8a3a59d677f5ec21ff8efe8017542c066
Adelson111 <adelson.oliveira111@gmail.com>
2019-07-08 10:44:40 -0300

    Faltou coisa

---
+++ Sistemas/Backend/SAPH/src/main/java/rest/Setor.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rest;
+
+import banco.Persistencia;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.util.List;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author adelson
+ */
+@Path("setor")
+public class Setor {
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String cadastrar(String json) {
+        return new Persistencia().cadastrar(new Gson().fromJson(json, modelo.Setor.class));
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("lista")
+    public String cadastrarLista(String json) {
+        return new Persistencia().cadastrar((List<Object>) new Gson().fromJson(json, new TypeToken<List<modelo.Setor>>() {
+        }.getType()));
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String atualizar(String json) {
+        return new Persistencia().atualizar(new Gson().fromJson(json, modelo.Setor.class));
+    }
+
+    @DELETE
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Path("{id}")
+    public String remover(@PathParam("id") long id) {
+        return new Persistencia().remover(new modelo.Setor(), id);
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String selecionar() {
+        return new Gson().toJson(new Persistencia().selecionar(new modelo.Setor()));
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{id}")
+    public String selecionar(@PathParam("id") long id) {
+        return new Gson().toJson(new Persistencia().selecionar(new modelo.Setor(), id));
+    }
+
+}
